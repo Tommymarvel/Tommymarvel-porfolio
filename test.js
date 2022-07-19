af305358c5df82e42fcf6b4982155eba784c7b58
Tommymarvel <99184522+Tommymarvel@users.noreply.github.com>
2022-07-19 15:56:04 +0100

    finalize mobile menu

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
-const hamburger = document.querySelector(".hamburger");
-const navTags = document.querySelector(".nav-tags");
+const hamburger = document.getElementById("hamburger");
+
+const closeMenu = document.getElementById("closemenu");
+
+const mobileMenu = document.querySelector(".mobile-menu");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navTags.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
 });
 
 document.querySelectorAll(".nav-link").forEach((n) =>
   n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navTags.classList.remove("active");
+    mobileMenu.classList.remove("active");
   })
+
 );
+closeMenu.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+});
