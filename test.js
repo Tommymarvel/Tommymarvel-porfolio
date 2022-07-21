3b40ebcc01779addec67a186bd12274dd6ab68a0
Tommymarvel <99184522+Tommymarvel@users.noreply.github.com>
2022-07-21 16:09:14 +0100

    add popup-modal

---
+++ test.js
@@ -0,0 +1,16 @@
+const hamburger = document.getElementById('hamburger');
+
+const closeMenu = document.getElementById('closemenu');
+
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
+}));
+closeMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+});
